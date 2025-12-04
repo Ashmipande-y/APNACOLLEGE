250b5eb7cf0b59b15eb6e0bcdd33ab28aa9caafb
Ashmipande-y <ashmipandey24@gmail.com>
2025-12-04 10:39:00 +0530

    Adding PATTERN1 Folder

---
+++ PATTERN1/Star.java
@@ -0,0 +1,15 @@
+package PATTERN1;
+
+
+import java.util.*;
+class Star{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int line=1;line<=4;line++){
+            for(int star=1;star<=line;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
